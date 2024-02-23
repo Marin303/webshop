e3666d5445fce82812a65844b1202e2de7c7faed
Marin <marin.muktic3@gmail.com>
2024-02-23 20:22:33 +0100

    layout

---
+++ src/Pages/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.scss";
+import Navbar from "../../Components/Navbar/Navbar";
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <Navbar />
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/Pages/Layout/Layout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Layout.scss";
+import Header from "../Header/Header";
+
+const Layout = () => {
+  return (
+    <div className="layout-container">
+      <Header />
+    </div>
+  );
+};
+
+export default Layout;
